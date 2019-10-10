14dbae5b94b279f37b57983ddcd6c740478720a0
Norlunn <martin@norlunn.net>
2019-10-10 20:39:30 +0200

    .

--- TestFiles/Test-KeywordCasing.ps1
+++ TestFiles/Test-KeywordCasing.ps1
@@ -1,14 +1,14 @@
 ﻿function Test-KeyWordCasing {
     $num = 1..3
     foreach ($n in $num) {
-        if ($n -EQ 2) {
+        if ($n -eq 2) {
             "Two"
         }
-        elseif($n -eq 3) {
+        elseif($n -eq 3 -or -not ($n -like "*OK*") -or $n -in 1..10 -or $n -match ("3" -repLace "4") -and $true) {
             "Three"
         }
         else{
-            "One!"
+            "One"
         }
     }
 }
