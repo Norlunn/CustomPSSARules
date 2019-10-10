99fb49fc6a3d85445cd8ee87fab6ca5de92d01f0
Norlunn <martin@norlunn.net>
2019-10-10 20:06:37 +0200

    .

--- Rules/CustomBundle.psm1
+++ Rules/CustomBundle.psm1
@@ -111,6 +111,25 @@ function Measure-ParameterPascal {
 }
 
 function Measure-KeywordLowercase {
+<#
+.SYNOPSIS
+    Keywords in PowerShell should be in lowercase.
+
+.DESCRIPTION
+    Keywords in PowerShell should be in lowercase.
+
+.EXAMPLE
+    Measure-KeywordLowercase -ScriptBlockAst $ScriptBlockAst
+
+.INPUTS
+    [System.Management.Automation.Language.ScriptBlockAst]
+
+.OUTPUTS
+    [Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
+
+.NOTES
+
+#>
     [CmdletBinding()]
     [OutputType([Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord])]
     param (
@@ -148,7 +167,6 @@ function Measure-KeywordLowercase {
                 [int]$startColumnNumber = $ast.Extent.StartColumnNumber
                 [int]$endColumnNumber = $ast.Extent.EndColumnNumber
                 [string]$correction = 'function' + $ast.Extent.Text.Substring(8, $ast.Extent.Text.Length - 8)
-                [string]$description = 'Useful but optional description text'
                 $correctionExtent = New-Object 'Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent' $startLineNumber,$endLineNumber,$startColumnNumber,$endColumnNumber,$correction,$description
                 $suggestedCorrections = New-Object System.Collections.ObjectModel.Collection['Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent']
                 [void]$suggestedCorrections.add($correctionExtent)
@@ -191,7 +209,6 @@ function Measure-KeywordLowercase {
                 [int]$startColumnNumber = $ast.Extent.StartColumnNumber
                 [int]$endColumnNumber = $ast.Extent.EndColumnNumber
                 [string]$correction = 'foreach' + $ast.Extent.Text.Substring(7, $ast.Extent.Text.Length - 7)
-                [string]$description = 'Useful but optional description text'
                 $correctionExtent = New-Object 'Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent' $startLineNumber,$endLineNumber,$startColumnNumber,$endColumnNumber,$correction,$description
                 $suggestedCorrections = New-Object System.Collections.ObjectModel.Collection['Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent']
                 [void]$suggestedCorrections.add($correctionExtent)
@@ -206,6 +223,58 @@ function Measure-KeywordLowercase {
             }
 
             #endregion foreach
+
+            #region if-else
+            [ScriptBlock]$Predicate = {
+                param ([System.Management.Automation.Language.Ast]$Ast)
+
+                [bool]$returnValue = $false
+
+                if ($Ast -is [System.Management.Automation.Language.IfStatementAst])
+                {
+                    [System.Management.Automation.Language.IfStatementAst]$IfAst = $Ast;
+                    if ($IfAst.Extent.Text.Substring(0, 2) -cne 'if')
+                    {
+                        $returnValue = $true
+                    }
+
+                    if ($IfAst.Extent.Text -like "*elseif*" -and $IfAst.Extent.Text -cnotlike "*elseif*")
+                    {
+                        $returnValue = $true
+                    }
+
+                    if ($IfAst.Extent.Text -match "else(\s)*{" -and $IfAst.Extent.Text -cnotmatch "else(\s)*{")
+                    {
+                        $returnValue = $true
+                    }
+                }
+
+                return $returnValue
+            }
+
+            [System.Management.Automation.Language.IfStatementAst[]]$Asts = $ScriptBlockAst.FindAll($Predicate, $false)
+
+            foreach ($Ast in $Asts)
+            {
+                [int]$startLineNumber =  $ast.Extent.StartLineNumber
+                [int]$endLineNumber = $ast.Extent.EndLineNumber
+                [int]$startColumnNumber = $ast.Extent.StartColumnNumber
+                [int]$endColumnNumber = $ast.Extent.EndColumnNumber
+                [string]$correction = $ast.Extent.Text -replace 'if', 'if' -replace 'elseif', 'elseif' -replace 'else', 'else'
+                $correctionExtent = New-Object 'Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent' $startLineNumber,$endLineNumber,$startColumnNumber,$endColumnNumber,$correction,$description
+                $suggestedCorrections = New-Object System.Collections.ObjectModel.Collection['Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent']
+                [void]$suggestedCorrections.add($correctionExtent)
+
+                New-Object -TypeName Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord -Property @{
+                    Message = "if-elseif-else keywords should all be in lowercase."
+                    Extent = $Ast.Extent
+                    RuleName = $PSCmdlet.MyInvocation.InvocationName
+                    Severity = 'Warning'
+                    SuggestedCorrections = $suggestedCorrections
+                }
+            }
+
+            #endregion if-else
         }
         catch {
             $_ | Select-Object -Property * | Out-File D:\repos\CustomPSSARules\error.txt -Force
--- Rules/Measure-KeywordLowercase.psm1
+++ Rules/Measure-KeywordLowercase.psm1
@@ -9,16 +9,26 @@ function Measure-KeywordLowercase {
 
     process {
         try {
-            #region foreach
-            [ScriptBlock]$Predicate2 = {
+            #region if-else
+            [ScriptBlock]$Predicate = {
                 param ([System.Management.Automation.Language.Ast]$Ast)
 
                 [bool]$returnValue = $false
 
-                if ($Ast -is [System.Management.Automation.Language.ForEachStatementAst])
+                if ($Ast -is [System.Management.Automation.Language.IfStatementAst])
                 {
-                    [System.Management.Automation.Language.ForEachStatementAst]$ForeachAst = $Ast;
-                    if ($ForeachAst.Extent.Text.Substring(0, 7) -cne 'foreach')
+                    [System.Management.Automation.Language.IfStatementAst]$IfAst = $Ast;
+                    if ($IfAst.Extent.Text.Substring(0, 2) -cne 'if')
+                    {
+                        $returnValue = $true
+                    }
+
+                    if ($IfAst.Extent.Text -like "*elseif*" -and $IfAst.Extent.Text -cnotlike "*elseif*")
+                    {
+                        $returnValue = $true
+                    }
+
+                    if ($IfAst.Extent.Text -match "else(\s)*{" -and $IfAst.Extent.Text -cnotmatch "else(\s)*{")
                     {
                         $returnValue = $true
                     }
@@ -27,30 +37,29 @@ function Measure-KeywordLowercase {
                 return $returnValue
             }
 
-            [System.Management.Automation.Language.ForEachStatementAst[]]$Asts2 = $ScriptBlockAst.FindAll($Predicate2, $false)
+            [System.Management.Automation.Language.IfStatementAst[]]$Asts = $ScriptBlockAst.FindAll($Predicate, $false)
 
-            foreach ($Ast in $Asts2)
+            foreach ($Ast in $Asts)
             {
-                <# [int]$startLineNumber =  $ast.Extent.StartLineNumber
+                [int]$startLineNumber =  $ast.Extent.StartLineNumber
                 [int]$endLineNumber = $ast.Extent.EndLineNumber
                 [int]$startColumnNumber = $ast.Extent.StartColumnNumber
                 [int]$endColumnNumber = $ast.Extent.EndColumnNumber
-                [string]$correction = 'foreach' + $ast.Extent.Text.Substring(7, $ast.Extent.Text.Length - 7)
-                [string]$description = 'Useful but optional description text'
+                [string]$correction = $ast.Extent.Text -replace 'if', 'if' -replace 'elseif', 'elseif' -replace 'else', 'else'
                 $correctionExtent = New-Object 'Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent' $startLineNumber,$endLineNumber,$startColumnNumber,$endColumnNumber,$correction,$description
                 $suggestedCorrections = New-Object System.Collections.ObjectModel.Collection['Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent']
-                [void]$suggestedCorrections.add($correctionExtent) #>
+                [void]$suggestedCorrections.add($correctionExtent)
 
                 New-Object -TypeName Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord -Property @{
-                    Message = "Foreach $($Asts2.Count) definition keyword 'foreach' should be all lowercase for '$($Ast.Extent.Text -split "`n" | Select-Object -First 1)'"
+                    Message = "if-elseif-else keywords should all be in lowercase."
                     Extent = $Ast.Extent
                     RuleName = $PSCmdlet.MyInvocation.InvocationName
                     Severity = 'Warning'
-                    #SuggestedCorrections = $suggestedCorrections
+                    SuggestedCorrections = $suggestedCorrections
                 }
             }
 
-            #endregion foreach
+            #endregion if-else
         }
         catch {
             $_ | Select-Object -Property * | Out-File D:\repos\CustomPSSARules\error.txt -Force
